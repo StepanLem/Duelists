8180b288d8ad3f0e64776996c59eac6dae8c972a
D9eka <120364200+D9eka@users.noreply.github.com>
2024-08-31 10:55:46 +0500

    WIP: сделать bootstrap сцену
    Эта версия без использования zenject.
    
    Реализован переход между сценами Boot, MainMenu и Gameplay.
    
    Добавлен экран загрузки при переходе между сценами.
    
    Сделана передача данных между сценами. В виде классов enterParams и exitParams для каждой сцены.
    
    Issues: #3 #4

---
+++ Assets/! Internal Assets/Architecture/GameRoot/Scenes.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace Assets.__Internal_Assets.Architecture
+{
+    public static class Scenes
+    {
+        public const string GAMEPLAY = "Gameplay";
+        public const string MAIN_MENU = "MainMenu";
+        public const string BOOT = "Boot";
+    }
+}
