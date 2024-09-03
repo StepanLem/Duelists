3d8d691beb87e915e4de7d6dbed3ba46a74bbe98
D9eka <120364200+D9eka@users.noreply.github.com>
2024-09-03 22:55:04 +0500

    WIP: сделать bootstrap сцену
    - Объединил отдельные скрипты для сцен в один вид (SceneEntryPoint, UISceneRootBinder)

--- Assets/! Internal Assets/Scenes/Scripts/SceneNames.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Scenes
-{
-    public static class SceneNames
-    {
-        public const string GAMEPLAY = "Gameplay";
-        public const string MAIN_MENU = "MainMenu";
-        public const string BOOT = "Boot";
-    }
-}
