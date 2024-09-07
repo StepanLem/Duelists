328528ae13c0a1c50ce9cd3eb8ef55c60b8f5e2b
D9eka <120364200+D9eka@users.noreply.github.com>
2024-09-07 14:58:47 +0500

    Add new Scenes to SceneRegistry

--- Assets/! Internal Assets/SceneArchitectureScripts/SceneRegistry/SceneRegistry.cs
+++ Assets/! Internal Assets/SceneArchitectureScripts/SceneRegistry/SceneRegistry.cs
@@ -5,6 +5,8 @@ public class SceneRegistry
 {
     public static SceneField PlayerVRScene { get; private set; }
     public static SceneField Bootstrap { get; private set; }
+    public static SceneField MainMenu { get; private set; }
+    public static SceneField Loading {  get; private set; }
 
     public static void InitializeFromScriptableObject()
     {
@@ -12,6 +14,8 @@ public class SceneRegistry
 
         PlayerVRScene = initParams.PlayerVRScene;
         Bootstrap = initParams.Bootstrap;
+        MainMenu = initParams.MainMenu;
+        Loading = initParams.Loading;
 
         Resources.UnloadAsset(initParams);
     }
--- Assets/! Internal Assets/SceneArchitectureScripts/SceneRegistry/SceneRegistrySO/SceneRegistrySO.cs
+++ Assets/! Internal Assets/SceneArchitectureScripts/SceneRegistry/SceneRegistrySO/SceneRegistrySO.cs
@@ -6,7 +6,11 @@ public class SceneRegistrySO : ScriptableObject
 {
     [SerializeField] private SceneField _playerVRScene;
     [SerializeField] private SceneField _bootstrap;
+    [SerializeField] private SceneField _mainMenu;
+    [SerializeField] private SceneField _loading;
 
     public SceneField PlayerVRScene => _playerVRScene;
     public SceneField Bootstrap => _bootstrap;
+    public SceneField MainMenu => _mainMenu;
+    public SceneField Loading => _loading;
 }
