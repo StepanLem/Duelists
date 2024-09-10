304cce7cd906edc38cb5b4038474312e800d9fd6
D9eka <120364200+D9eka@users.noreply.github.com>
2024-09-10 16:08:13 +0500

    Fix scene loading

---
+++ Assets/! Internal Assets/UI/MainMenuStartGameplayButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MainMenuStartGameplayButton : MonoBehaviour
+{
+    public void StartGameplay()
+    {
+        LoadingScreenController.AddSceneToLoadOnNextLoadingScreen(SceneRegistry.GameplayScene);
+        LoadingScreenController.AddSceneToUnloadOnNextLoadingScreen(SceneRegistry.MainMenuScene);
+        LoadingScreenController.InvokeLoadingScreen(SceneRegistry.GameplayScene);
+    }
+}
