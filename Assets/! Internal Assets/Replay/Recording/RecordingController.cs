fd2e396538fcc7507f9e5230bb3946f2f276710c
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-09-30 02:57:36 +0500

    Implementation for Recording

---
+++ Assets/! Internal Assets/Replay/Recording/RecordingController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class RecordingController<T> : IRecordingController<T>
+{
+    private readonly IReplayWriter<T> _recordingReel;
+    private readonly Func<T> _valueGetter;
+    private readonly ITicker _scheduler;
+
+    public RecordingController(IReplayWriter<T> recordingReel, Func<T> valueGetter, ITicker ticker)
+    {
+        _recordingReel = recordingReel;
+        _scheduler = ticker;
+        _valueGetter = valueGetter;
+    }
+
+    public bool IsPaused { get; private set; } = true;
+
+    private void TakeSnapshot()
+    {
+        _recordingReel.TakeSnapshot(_valueGetter.Invoke());
+    }
+
+    public void Start()
+    {
+        IsPaused = false;
+        _scheduler.Fire += TakeSnapshot;
+    }
+
+    public void Stop()
+    {
+        IsPaused = false;
+        _scheduler.Fire -= TakeSnapshot;
+    }
+
+    public IReplayReader<T> GetReplay()
+    {
+        return _recordingReel.GetReplay();
+    }
+}
---
+++ Assets/! Internal Assets/Replay/Recording/ReplayWriter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using static UnityEngine.Rendering.DebugUI;
+
+public class ReplayWriter<T> : IReplayWriter<T>
+{
+    private readonly IInterpolator<T> _interpolator;
+    private readonly List<Snapshot<T>> _frames = new ();
+
+    private T _cachedValue;
+    private DateTime _cachedTime;
+    private int _skipCounter;
+
+    public ReplayWriter(IInterpolator<T> interpolator)
+    {
+        _interpolator = interpolator;
+    }
+
+    public IReplayReader<T> GetReplay()
+    {
+        DropCachedFrame();
+        return new ReplayReader<T>(_interpolator, _frames);
+    }
+
+    public void TakeSnapshot(T value)
+    {
+        if (value.Equals(_cachedValue))
+        {
+            _skipCounter++;
+            _cachedTime = DateTime.UtcNow;
+            return;
+        }
+        else
+        {
+            DropCachedFrame();
+            _cachedValue = value;
+        }
+        AddFrame(value, DateTime.UtcNow);
+    }
+
+    private void DropCachedFrame()
+    {
+        if (_skipCounter == 0)
+        {
+            return;
+        }
+        AddFrame(_cachedValue, _cachedTime);
+        _skipCounter = 0;
+    }
+
+    private void AddFrame(T value, DateTime dateTime)
+    {
+        _frames.Add(new Snapshot<T>(value, dateTime));
+    }
+}
