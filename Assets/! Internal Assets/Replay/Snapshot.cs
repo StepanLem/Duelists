0ce751138887b679c6db9cb6e6bb475daf012ff2
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-09-30 03:04:33 +0500

    Snapshot class for storing data with timestamp

---
+++ Assets/! Internal Assets/Replay/Snapshot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public readonly struct Snapshot<T>
+{
+    public readonly T Value;
+    public readonly DateTime TimeStamp;
+    public Snapshot(T value, DateTime timeStamp)
+    {
+        Value = value;
+        TimeStamp = timeStamp;
+    }
+}
