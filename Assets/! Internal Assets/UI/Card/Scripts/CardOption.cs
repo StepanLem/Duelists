1ebb4eb3c0912ec822d441704f24acafb3eeea26
D9eka <120364200+D9eka@users.noreply.github.com>
2024-10-03 11:02:15 +0500

    Add Cards

---
+++ Assets/! Internal Assets/UI/Card/Scripts/CardOption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class CardOption
+{
+    [SerializeField] private string _value;
+    [Space]
+    [SerializeField] private string _text;
+    [SerializeField] private Sprite _icon;
+
+    public string Value => _value;
+
+    public string Text => _text;
+    public Sprite Icon => _icon;
+}
