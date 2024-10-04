0482aeeef3125551bbd360c2ec1caaf6b33b90b5
D9eka <120364200+D9eka@users.noreply.github.com>
2024-10-04 19:39:20 +0500

    Add ResumeGameScreen to Gameplay.unity

---
+++ Assets/! Internal Assets/UI/Screens/ResumeGameScreen/Scripts/ResumeGameScreen.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ResumeGameScreen : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _delayText;
+    [Space]
+    [SerializeField] private int _delay = 5;
+    [SerializeField] private UnityEvent _afterDelay;
+
+    private void OnEnable()
+    {
+        _delayText.text = _delay.ToString();
+        StartCoroutine(DelayRoutine());
+    }
+
+    private IEnumerator DelayRoutine()
+    {
+        for (int i = 0; i < _delay; i++) 
+        { 
+            yield return new WaitForSeconds(1);
+            _delayText.text = (_delay - i - 1).ToString();
+        }
+
+        _afterDelay?.Invoke();
+    }
+}
