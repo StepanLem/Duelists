875bf4a93ece128eb1919c0db30ef4afe920c222
D9eka <120364200+D9eka@users.noreply.github.com>
2024-11-01 12:48:59 +0500

    Update MainMenu

---
+++ Assets/! Internal Assets/SceneArchitectureScripts/ProjectContext/ProjectInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Zenject;
+
+public class ProjectInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Container
+            .Bind<MatchManager>()
+            .FromNewComponentOnNewGameObject()
+            .WithGameObjectName(nameof(MatchManager))
+            .AsSingle();
+
+        Container
+            .Bind<DifficultySystem>()
+            .FromComponentInNewPrefabResource(nameof(DifficultySystem))
+            .AsSingle();
+    }
+}
--- Assets/! Internal Assets/UI/ConcreteUIElementMaterials/StartSoloGameScreen/Scripts/StartSoloGameScreen.cs
+++ Assets/! Internal Assets/UI/ConcreteUIElementMaterials/StartSoloGameScreen/Scripts/StartSoloGameScreen.cs
@@ -1,10 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 public class StartSoloGameScreen : MonoBehaviour
 {
+    [SerializeField] private SegmentedButton _difficultyButton;
+    [SerializeField] private SegmentedButton _roundsCountButton;
     [SerializeField] private SegmentedButton _enemyTypeButton;
     [SerializeField] private Button _chooseEnemyTypesButton;
+
+    private MatchManager _matchManager;
+    private DifficultySystem _difficultySystem;
+
+    [Inject]
+    public void Construct(MatchManager matchManager, DifficultySystem difficultySystem)
+    {
+        _matchManager = matchManager;
+        _difficultySystem = difficultySystem;
+    }
+
+    public void StartMatch()
+    {
+        int roundsCound = int.Parse(_roundsCountButton.Value);
+        RoundSO roundData = _difficultySystem.GetRoundByDifficulty(_difficultyButton.Value);
+        _matchManager.StarMatch(roundsCound, roundData);
+    }
 
     private void Start()
     {
