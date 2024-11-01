7f6831bc8341494831a729271acc8f485a2d6565
D9eka <120364200+D9eka@users.noreply.github.com>
2024-11-01 12:43:11 +0500

    Create DifficultySystem

---
+++ Assets/! Internal Assets/Match/Difficulty/DifficultySystem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DifficultySystem : MonoBehaviour
+{
+    [SerializeField] private RoundSO _easyRound;
+    [SerializeField] private RoundSO _mediumRound;
+    [SerializeField] private RoundSO _hardRound;
+
+    public RoundSO GetRoundByDifficulty(string difficultyTag)
+    {
+        return difficultyTag switch
+        {
+            "Easy" => _easyRound,
+            "Medium" => _mediumRound,
+            "Hard" => _hardRound,
+            _ => throw new System.NotImplementedException()
+        };
+    }
+}
