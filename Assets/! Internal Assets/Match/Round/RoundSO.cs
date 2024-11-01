04d9983b32f356409471c3365286dda41b9410c6
D9eka <120364200+D9eka@users.noreply.github.com>
2024-11-01 12:42:43 +0500

    Create RoundSO

---
+++ Assets/! Internal Assets/Match/Round/RoundSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoundSO", menuName = "Duelists/Round")]
+public class RoundSO : ScriptableObject
+{
+    [SerializeField] private float _beforeAttackTime;
+    [SerializeField] private float _attackTime;
+    [SerializeField] private float _afterAttackTime;
+
+    public float BeforeAttackTime => _beforeAttackTime;
+    public float AttackTime => _attackTime;
+    public float AfterAttackTime => _afterAttackTime;
+}
