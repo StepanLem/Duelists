a971f81f715c9f5a1e85305364ce79a626d6d731
D9eka <120364200+D9eka@users.noreply.github.com>
2024-11-15 20:20:17 +0500

    Update LoadingScreenController.cs

--- Assets/! Internal Assets/SceneArchitectureScripts/ProjectContext/LoadingScreenController.cs
+++ Assets/! Internal Assets/SceneArchitectureScripts/ProjectContext/LoadingScreenController.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public static class LoadingScreenController
 {
     private static SceneField _currentScene;
+    private static AsyncOperation _loadNewSceneOperation;
 
     public static event Action<float> OnLoadProgressChange;
 
@@ -23,44 +24,49 @@ public static class LoadingScreenController
 
     private static IEnumerator LoadSceneRoutine(SceneField newScene)
     {
-        Time.timeScale = 0f;
-
-        yield return AsyncProcessor.StartRoutine(LoadNewScene(SceneRegistry.LoadingScene));
+        yield return AsyncProcessor.StartRoutine(LoadNewSceneRoutine(SceneRegistry.LoadingScene));
         yield return new WaitUntil(() => LoadingScreen.Instance != null);
         yield return AsyncProcessor.StartRoutine(LoadingScreen.Instance.FadeIn(_currentScene == null));
 
         if (_currentScene != null)
         {
-            yield return AsyncProcessor.StartRoutine(UnloadScene(_currentScene));
+            yield return AsyncProcessor.StartRoutine(UnloadSceneRoutine(_currentScene));
         }
-        yield return AsyncProcessor.StartRoutine(LoadNewScene(newScene, true));
+        yield return AsyncProcessor.StartRoutine(LoadNewSceneRoutine(newScene, true));
+        _loadNewSceneOperation.allowSceneActivation = false;
 
         yield return AsyncProcessor.StartRoutine(LoadingScreen.Instance.FadeOut());
-        yield return AsyncProcessor.StartRoutine(UnloadScene(SceneRegistry.LoadingScene));
-
-        Time.timeScale = 1f;
+        yield return AsyncProcessor.StartRoutine(UnloadSceneRoutine(SceneRegistry.LoadingScene));
+        yield return new WaitForSecondsRealtime(1f);
 
         _currentScene = newScene;
     }
 
-    private static IEnumerator LoadNewScene(SceneField newScene, bool showLoadingProgress = false)
+    private static IEnumerator LoadNewSceneRoutine(SceneField newScene, bool showLoadingProgress = false)
     {
-        AsyncOperation loadNewScene = SceneManager.LoadSceneAsync(newScene.BuildIndex, LoadSceneMode.Additive);
-        while (!loadNewScene.isDone)
-        {
-            if (showLoadingProgress)
-            {
-                OnLoadProgressChange?.Invoke(loadNewScene.progress);
-            }
-            yield return new WaitForEndOfFrame();
-        }
+        AsyncOperation loadNewSceneOperation = SceneManager.LoadSceneAsync(newScene.BuildIndex, LoadSceneMode.Additive);
+        _loadNewSceneOperation = loadNewSceneOperation;
         if (showLoadingProgress)
         {
-            OnLoadProgressChange?.Invoke(loadNewScene.progress);
+            yield return AsyncProcessor.StartRoutine(ShowLoadingProgressRoutine(loadNewSceneOperation));
+        }
+        else
+        {
+            yield return new WaitUntil(() => loadNewSceneOperation.isDone);
         }
     }
 
-    private static IEnumerator UnloadScene(SceneField sceneToUnload)
+    private static IEnumerator ShowLoadingProgressRoutine(AsyncOperation loadNewSceneOperation)
+    {
+        while (!loadNewSceneOperation.isDone)
+        {
+            OnLoadProgressChange?.Invoke(loadNewSceneOperation.progress);
+            yield return new WaitForEndOfFrame();
+        }
+        OnLoadProgressChange?.Invoke(loadNewSceneOperation.progress);
+    }
+
+    private static IEnumerator UnloadSceneRoutine(SceneField sceneToUnload)
     {
         AsyncOperation unloadCurrentSceneOperation = SceneManager.UnloadSceneAsync(sceneToUnload.BuildIndex);
         yield return new WaitUntil(() => unloadCurrentSceneOperation.isDone);
