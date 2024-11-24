72ec2db58d5eeefd838fad7740cec00203006650
Stepan Lemtyugin <stepanlemtugin@gmail.com>
2024-11-24 23:28:08 +0500

    Start Add Angular Limits

--- Assets/! Internal Assets/Sword and Scabbard/SwordAndScabbardInteractionSystem.cs
+++ Assets/! Internal Assets/Sword and Scabbard/SwordAndScabbardInteractionSystem.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 //TODO Firstly:
-//1.Стабилизировать ножны.(чтобы были так же спокойны, как меч)
-//2.Убрать излишнее кручение ножен при входе/выходе меча. + сделать более плавное возвращение к своей изначальной позиции
-//3.Попробовать держать ножны fixed джоинтом
-//4.(ctrl+f)человек может хватать и отпускать меч много раз. Надо сделать обработку событий.
+//1. Надо бы сделать хелпер, чтобы он плавно развернул меч к нужному углу поворота.
+//2. Есть пролаги во время входа в ножны и выхода. Возможно из-за того, что меняем transform.position, а не rb.position. Или почему-то другому. Надо разобраться.
+//3. Сделать разные editorSword для отслеживания разных лимитов.
 
 //TODO Secondly:
 //1. Оптимизировать вычисления. В настройках физики; в настройках джоинтов; в частоте обновления корутины.
@@ -22,6 +22,7 @@ public class SwordAndScabbardInteractionSystem : MonoBehaviour
 
     [SerializeField] private float _exitFillAmount;
 
+    [SerializeField] private Quaternion _defaultSwordTipRotation;
     [SerializeField] private List<SwordInScabbardKeyframe> Keyframes;
 
     /// Нужен, чтобы можно было свободно изменять пивот ножен.
@@ -31,10 +32,13 @@ public class SwordAndScabbardInteractionSystem : MonoBehaviour
     [Tooltip("Mouth - устье ножен (aka место, куда заходит острие меча в самом начале)")]
     [SerializeField] private OnTriggerComponent MouthTriggerChecker;
 
+    [SerializeField] internal XRGrabInteractable _xrGrabInteractable;
+    [SerializeField] internal Rigidbody _rigidody;
     [SerializeField] private Collider[] _scabbardColliders;
 
+    [System.NonSerialized]
     [Tooltip("Меч, что сейчас в ножнах")]
-    private Sword _sword;
+    internal Sword _sword;
 
     [Tooltip("Джоинт, ограничивающий перемещение и повороты меча, как это делали бы реальные ножны, НО дающие мечу вывалиться из конца ножен.")]
     private ConfigurableJoint _jointScabbardSidesCosplayer;
@@ -45,9 +49,46 @@ public class SwordAndScabbardInteractionSystem : MonoBehaviour
     private SwordInScabbardKeyframe _currentKeyframe;
     private int _currentKeyframeIndex;
 
+    public bool spawnSwordOnStart;
+    [SerializeField] private GameObject _swordPrefab;
+
+    private SwordScabbardState _currentState;
+    internal OnlySwordHolded _onlySwordHolded;
+    internal OnlySkabbardHolded _onlySсabbardHolded;
+    internal BothAreHolded _bothAreHolded;
+    internal BothNotHolded _bothNotHolded;
+
     public void Awake()
     {
+        _onlySwordHolded = new(this);
+        _onlySсabbardHolded = new(this);
+        _bothAreHolded = new(this);
+        _bothNotHolded = new(this);
+
         MouthTriggerChecker.OnEnter += OnMouthTriggerEnter;
+
+
+        if (spawnSwordOnStart)
+        {
+            var swordGameObject = Instantiate(_swordPrefab, this.transform.parent);
+            if (!swordGameObject.TryGetComponent<Sword>(out var sword))
+            {
+                Debug.LogError("Sword component is missing on prefab!");
+                return;
+            }
+
+            // Позиция
+            var localRememberedSwordTipPosition = Keyframes[^1].SwordTipPosition;
+            var targetTipWorldPosition = _origin.TransformPoint(localRememberedSwordTipPosition);
+            sword.transform.position += targetTipWorldPosition - sword.Tip.position;
+
+            // Вращение
+            Quaternion targetSwordTipRotation = _origin.rotation * Keyframes[^1].SwordTipRotation;
+            Quaternion swordTipToTargetOffset = Quaternion.Inverse(sword.Tip.rotation) * targetSwordTipRotation;
+            sword.transform.rotation = sword.transform.rotation * swordTipToTargetOffset;
+
+            StartEnteringInScabbard(sword);
+        }
     }
 
     private void OnMouthTriggerEnter(Collider collider)
@@ -80,7 +121,10 @@ public class SwordAndScabbardInteractionSystem : MonoBehaviour
                 Physics.IgnoreCollision(swordCollider, scabbardCollider);
         }
 
-        StopTrackingRotationWhileInScabbard(_sword.XRGrabInteractable);
+        //Повороты в зависимости от способа хватания надо занулять. Но тогда хуйня какая-то
+        /*_sword.XRGrabInteractable.trackRotation = false;
+        this._xrGrabInteractable.trackRotation = false;*/
+
 
         //Устанавливаем для меча начальный угол поворота.
         // Вычисляем целевое вращение для меча, чтобы кончик оказался на запомненном относительном Origin
@@ -95,13 +139,27 @@ public class SwordAndScabbardInteractionSystem : MonoBehaviour
         SetupScabbardEndJoint();
         SetupScabbardSidesJoint();
 
+        if (_xrGrabInteractable.isSelected && _sword.XRGrabInteractable.isSelected)
+            _currentState = _bothAreHolded;
+        else if (_xrGrabInteractable.isSelected)
+            _currentState = _onlySсabbardHolded;
+        else if (_sword.XRGrabInteractable.isSelected)
+            _currentState = _onlySwordHolded;
+        else
+            _currentState = _bothNotHolded;
+
+        _currentState.EnterState();
+        Debug.Log("при заходе" + _currentState.GetType());
+
         StartCoroutine(nameof(PerformeKeyframeSwitchingAndLogic));
     }
 
-    private void StopTrackingRotationWhileInScabbard(XRGrabInteractable grabbable)
+    internal void SwitchState(SwordScabbardState newState)
     {
-        //TODO: человек может хватать и отпускать меч много раз. Надо сделать обработку событий.
-        grabbable.trackRotation = false;
+        _currentState.ExitState();
+        _currentState = newState;
+        Debug.Log(_currentState.GetType());
+        _currentState.EnterState();
     }
 
     private void SetupScabbardEndJoint()
@@ -143,9 +201,9 @@ public class SwordAndScabbardInteractionSystem : MonoBehaviour
         _jointScabbardSidesCosplayer.xMotion = ConfigurableJointMotion.Locked;
         _jointScabbardSidesCosplayer.yMotion = ConfigurableJointMotion.Locked;
         _jointScabbardSidesCosplayer.zMotion = ConfigurableJointMotion.Free; //свободно, потому что ограничивается другим джоинтом
-        _jointScabbardSidesCosplayer.angularYMotion = ConfigurableJointMotion.Locked;
-        _jointScabbardSidesCosplayer.angularXMotion = ConfigurableJointMotion.Locked;
-        _jointScabbardSidesCosplayer.angularZMotion = ConfigurableJointMotion.Locked;
+        _jointScabbardSidesCosplayer.angularYMotion = ConfigurableJointMotion.Limited;
+        _jointScabbardSidesCosplayer.angularXMotion = ConfigurableJointMotion.Limited;
+        _jointScabbardSidesCosplayer.angularZMotion = ConfigurableJointMotion.Locked;//не разрешается менять, так как начинается тряска если освобождены все 3
     }
 
     private IEnumerator PerformeKeyframeSwitchingAndLogic()
@@ -154,7 +212,7 @@ public class SwordAndScabbardInteractionSystem : MonoBehaviour
         {
             FillAmount = СalculateFillAmount(_sword, Keyframes[0].SwordTipPosition, Keyframes[^1].SwordTipPosition);
 
-            Debug.Log(FillAmount);
+            //Debug.Log(FillAmount);
 
             if (FillAmount < _exitFillAmount)
             {
@@ -172,6 +230,7 @@ public class SwordAndScabbardInteractionSystem : MonoBehaviour
             if (_currentKeyframeIndex != Keyframes.Count - 1)
                 LerpConfigurationBetweenKeyframes(_currentKeyframe, Keyframes[_currentKeyframeIndex + 1]);
 
+            //yield return new WaitForFixedUpdate();
             yield return null;
         }
     }
@@ -242,10 +301,39 @@ public class SwordAndScabbardInteractionSystem : MonoBehaviour
         var localRememberedSwordTipPosition = Vector3.Lerp(currentKeyframe.SwordTipPosition, nextKeyframe.SwordTipPosition, stepBetweenKeyframes);
         targetWorldPosition = _origin.TransformPoint(localRememberedSwordTipPosition);
         _jointScabbardSidesCosplayer.anchor = this.transform.InverseTransformPoint(targetWorldPosition);
+
+        //Rotation limits
+
+        // Интерполируем углы
+        Vector3 interpolatedAngularLimits = new Vector3(
+            Mathf.Lerp(currentKeyframe.AngularLimits.x, nextKeyframe.AngularLimits.x, stepBetweenKeyframes),
+            Mathf.Lerp(currentKeyframe.AngularLimits.y, nextKeyframe.AngularLimits.y, stepBetweenKeyframes),
+            Mathf.Lerp(currentKeyframe.AngularLimits.z, nextKeyframe.AngularLimits.z, stepBetweenKeyframes)
+        );
+
+        SoftJointLimit limit = new();
+
+        // Ограничение углов по оси X
+        limit.limit = interpolatedAngularLimits.x;
+        _jointScabbardSidesCosplayer.lowAngularXLimit = limit;
+        _jointScabbardSidesCosplayer.highAngularXLimit = limit;
+
+        // Ограничение углов по оси Y
+        limit.limit = interpolatedAngularLimits.y;
+        _jointScabbardSidesCosplayer.angularYLimit = limit;
+
+        // Ограничение углов по оси Z
+        limit.limit = interpolatedAngularLimits.z;
+        _jointScabbardSidesCosplayer.angularZLimit = limit;
     }
 
     private void SwordExitScabbard()
     {
+        _currentState.ExitState();
+
+        _sword.XRGrabInteractable.trackRotation = true;
+        this._xrGrabInteractable.trackRotation = true;
+
         foreach (Collider swordCollider in _sword.Colliders)
         {
             foreach (Collider scabbardCollider in _scabbardColliders)
@@ -274,16 +362,91 @@ public class SwordAndScabbardInteractionSystem : MonoBehaviour
         else if (Keyframes.Count == 1) fillAmount = 1f;
         else fillAmount = СalculateFillAmount(_swordComponentForKeyframesCreation, Keyframes[0].SwordTipPosition, Keyframes[^1].SwordTipPosition);
 
+        //Устанавливаем лимиты с меча для лимитов.
+        Quaternion currentRotation_editorAngleLimitsSword = Quaternion.Inverse(_origin.rotation) * _swordForAngleLimits.Tip.rotation;
+        Quaternion deviation = Quaternion.Inverse(_defaultSwordTipRotation) * currentRotation_editorAngleLimitsSword;
+        var angularLimits = NormalizeAngles(deviation.eulerAngles);
+
         SwordInScabbardKeyframe newKeyframe = new()
         {
             SwordTipPosition = _origin.InverseTransformPoint(_swordComponentForKeyframesCreation.Tip.position),
             SwordTipRotation = Quaternion.Inverse(_origin.rotation) * _swordComponentForKeyframesCreation.Tip.rotation,
-            FillAmount = fillAmount
+            FillAmount = fillAmount,
+            AngularLimits = angularLimits
         };
 
         Keyframes.Add(newKeyframe);
 
         Keyframes.Sort((a, b) => a.FillAmount.CompareTo(b.FillAmount));
+    }
+
+    [ContextMenu("SetDefaultSwordTipRotation")]
+    public void SetDefaultSwordTipRotation()
+    {
+        _defaultSwordTipRotation = Quaternion.Inverse(_origin.rotation) * _swordComponentForKeyframesCreation.Tip.rotation;
+    }
+
+    [ContextMenu("NullifyAllLimitsIfLow")]
+    public void NullifyAllLimitsIfLow()
+    {
+        for (int i = 0; i < Keyframes.Count; i++)
+        {
+            var kf = Keyframes[i];
+
+            kf.AngularLimits = new Vector3(
+            Mathf.Max(0, kf.AngularLimits.x),
+            Mathf.Max(0, kf.AngularLimits.y),
+            Mathf.Max(0, kf.AngularLimits.z));
+
+            Keyframes[i] = kf;
+        }
+    }
+
+    [SerializeField] private int _editorKeyframeIndex;
+    [SerializeField] private Sword _swordForAngleLimits;
+
+    [ContextMenu("SetKeyframeDataToEditingSword")]
+    public void SetKeyframeDataToEditingSword()
+    {
+        //Position
+        var localRememberedSwordTipPosition = Keyframes[_editorKeyframeIndex].SwordTipPosition;
+        var targetTipWorldPosition = _origin.TransformPoint(localRememberedSwordTipPosition);
+        _swordComponentForKeyframesCreation.transform.position += targetTipWorldPosition - _swordComponentForKeyframesCreation.Tip.position;
+
+        //Rotation
+        Quaternion targetSwordTipRotation = _origin.rotation * Keyframes[_editorKeyframeIndex].SwordTipRotation;
+        Quaternion swordTipToTargetOffset = Quaternion.Inverse(_swordComponentForKeyframesCreation.Tip.rotation) * targetSwordTipRotation;
+        _swordComponentForKeyframesCreation.transform.rotation = _swordComponentForKeyframesCreation.transform.rotation * swordTipToTargetOffset;
+    }
+
+    [ContextMenu("UpdateAngularLimitsForKeyframe")]
+    public void UpdateAngularLimitsForKeyframe()
+    {
+        Quaternion currentRotation = Quaternion.Inverse(_origin.rotation) * _swordForAngleLimits.Tip.rotation;
+        Quaternion deviation = Quaternion.Inverse(_defaultSwordTipRotation) * currentRotation;
+
+        // Преобразуем к углам Эйлера
+        var angularLimits = NormalizeAngles(deviation.eulerAngles);
+
+        var keyframe = Keyframes[_editorKeyframeIndex];
+        keyframe.AngularLimits = angularLimits;
+        Keyframes[_editorKeyframeIndex] = keyframe;
+    }
+
+    // Нормализация углов в диапазон [-180, 180]
+    private Vector3 NormalizeAngles(Vector3 angles)
+    {
+        angles.x = NormalizeAngle(angles.x);
+        angles.y = NormalizeAngle(angles.y);
+        angles.z = NormalizeAngle(angles.z);
+        return angles;
+    }
+
+    private float NormalizeAngle(float angle)
+    {
+        if (angle > 180f) angle -= 360f;
+        if (angle < -180f) angle += 360f;
+        return angle;
     }
 #endif
 
@@ -300,12 +463,6 @@ public struct SwordInScabbardKeyframe
     public Vector3 SwordTipPosition;
     public Quaternion SwordTipRotation;
 
-    //public JointConfiguration JointConfiguration;
+    [Tooltip("Максимальное отклонение угла в градусах.")]
+    public Vector3 AngularLimits;
 }
-
-/*[System.Serializable]
-public class JointConfiguration
-{
-    public int XandZAngleLimit;
-
-}*/
---
+++ Assets/! Internal Assets/Sword and Scabbard/SwordScabbardStates/SwordScabbardState.cs
@@ -0,0 +1,236 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public abstract class SwordScabbardState
+{
+    protected SwordAndScabbardInteractionSystem _context;
+
+    public SwordScabbardState(SwordAndScabbardInteractionSystem context)
+    {
+        _context = context;
+    }
+    public abstract void EnterState();
+    public abstract void ExitState();
+    public abstract void UpdateState();
+
+
+    protected void SwitchStateToNotHolding(SelectEnterEventArgs arg0)
+    {
+        _context.SwitchState(_context._bothNotHolded);
+    }
+    protected void SwitchStateToNotHolding(SelectExitEventArgs arg0)
+    {
+        _context.SwitchState(_context._bothNotHolded);
+    }
+    protected void SwitchStateToBothAreHolded(SelectEnterEventArgs arg0)
+    {
+        _context.SwitchState(_context._bothAreHolded);
+    }
+    protected void SwitchStateToBothAreHolded(SelectExitEventArgs arg0)
+    {
+        _context.SwitchState(_context._bothAreHolded);
+    }
+    protected void SwitchStateToOnlyScabbardHolded(SelectEnterEventArgs arg0)
+    {
+        _context.SwitchState(_context._onlySсabbardHolded);
+    }
+    protected void SwitchStateToOnlyScabbardHolded(SelectExitEventArgs arg0)
+    {
+        _context.SwitchState(_context._onlySсabbardHolded);
+    }
+    protected void SwitchStateToOnlySwordHolded(SelectEnterEventArgs arg0)
+    {
+        _context.SwitchState(_context._onlySwordHolded);
+    }
+    protected void SwitchStateToOnlySwordHolded(SelectExitEventArgs arg0)
+    {
+        _context.SwitchState(_context._onlySwordHolded);
+    }
+
+    /*private IEnumerator ReturnToStartPositionSmoothly(XRGrabInteractable grabbable)
+    {
+        //Мб здесь тогда надо просто фиксировать. fillAmount. А не через замедление работать
+        //grabbable.angularVelocityScale = 0.05f; // Установить замедление
+        grabbable.TryGetComponent<Rigidbody>(out var rb);
+        rb.maxLinearVelocity = 0.1f;
+        rb.maxAngularVelocity = 0.1f;
+        yield return new WaitForSeconds(2f);
+        rb.maxLinearVelocity = 35f;
+        rb.maxAngularVelocity = 35f;
+        //grabbable.angularVelocityScale = 1f; // Вернуть нормальную скорость
+    }*/
+}
+
+
+public class OnlySwordHolded : SwordScabbardState
+{
+    private Coroutine _returnToStartPositionRoutine;
+
+    public OnlySwordHolded(SwordAndScabbardInteractionSystem context) : base(context)
+    {
+    }
+
+    public override void EnterState()
+    {
+        _context._xrGrabInteractable.selectEntered.AddListener(SwitchStateToBothAreHolded);
+        _context._sword.XRGrabInteractable.selectExited.AddListener(SwitchStateToNotHolding);
+
+        _context._sword.XRGrabInteractable.trackRotation = false;//временно для теста.
+        //_returnToStartPositionRoutine = _context.StartCoroutine(AAA());
+    }
+
+    /*public IEnumerator AAA()
+    {
+        
+    }*/
+
+    public override void ExitState()
+    {
+        _context._xrGrabInteractable.selectEntered.RemoveListener(SwitchStateToBothAreHolded);
+        _context._sword.XRGrabInteractable.selectExited.RemoveListener(SwitchStateToNotHolding);
+
+        _context._sword.XRGrabInteractable.trackRotation = true;
+
+        if (_returnToStartPositionRoutine != null)//А она здесь точно null?
+        {
+            _context.StopCoroutine(_returnToStartPositionRoutine);
+            _context._sword.Rigidbody.maxLinearVelocity = 35f;
+            _context._sword.Rigidbody.maxAngularVelocity = 35f;
+        }
+    }
+
+    public override void UpdateState()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class OnlySkabbardHolded : SwordScabbardState
+{
+    private Coroutine _returnToStartPositionRoutine;
+
+    public OnlySkabbardHolded(SwordAndScabbardInteractionSystem context) : base(context)
+    {
+    }
+
+    public override void EnterState()
+    {
+        _context._sword.XRGrabInteractable.selectEntered.AddListener(SwitchStateToBothAreHolded);
+        _context._xrGrabInteractable.selectExited.AddListener(SwitchStateToNotHolding);
+
+        _returnToStartPositionRoutine = _context.StartCoroutine(SmoothlyReturnToOldPosition());
+    }
+
+    private FixedJoint fixedJoint;
+    public IEnumerator SmoothlyReturnToOldPosition()
+    {
+        fixedJoint = _context.gameObject.AddComponent<FixedJoint>();
+        fixedJoint.connectedBody = _context._sword.Rigidbody;
+        //вернуть ножны в руку плавно
+        /*_context._rigidody.maxLinearVelocity = 5f;
+        _context._rigidody.maxAngularVelocity = 5f;*/
+        /*_context._sword.Rigidbody.maxLinearVelocity = 5f;
+        _context._sword.Rigidbody.maxAngularVelocity = 5f;*/
+        yield return new WaitForSeconds(1f);
+
+        Component.Destroy(fixedJoint);
+        /*_context._sword.Rigidbody.maxLinearVelocity = 35f;
+        _context._sword.Rigidbody.maxAngularVelocity = 35f;*/
+    }
+
+    public override void ExitState()
+    {
+        _context._sword.XRGrabInteractable.selectEntered.RemoveListener(SwitchStateToBothAreHolded);
+        _context._xrGrabInteractable.selectExited.RemoveListener(SwitchStateToNotHolding);
+
+        if (_returnToStartPositionRoutine != null)//А она здесь точно null?
+        {
+            _context.StopCoroutine(_returnToStartPositionRoutine);
+            if (fixedJoint != null)
+                Component.Destroy(fixedJoint);
+            _context._rigidody.maxLinearVelocity = 35f;
+            _context._rigidody.maxAngularVelocity = 35f;
+        }
+    }
+
+    public override void UpdateState()
+    {
+
+    }
+}
+
+public class BothAreHolded : SwordScabbardState
+{
+    public BothAreHolded(SwordAndScabbardInteractionSystem context) : base(context)
+    {
+    }
+
+    public override void EnterState()
+    {
+        _context._sword.XRGrabInteractable.selectExited.AddListener(SwitchStateToOnlyScabbardHolded);
+        _context._xrGrabInteractable.selectExited.AddListener(SwitchStateToOnlySwordHolded);
+
+        _context._sword.XRGrabInteractable.trackRotation = false;
+
+    }
+
+    public override void ExitState()
+    {
+        _context._sword.XRGrabInteractable.selectExited.RemoveListener(SwitchStateToOnlyScabbardHolded);
+        _context._xrGrabInteractable.selectExited.RemoveListener(SwitchStateToOnlySwordHolded);
+
+        _context._sword.XRGrabInteractable.trackRotation = true;
+    }
+
+    public override void UpdateState()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class BothNotHolded : SwordScabbardState
+{
+    public BothNotHolded(SwordAndScabbardInteractionSystem context) : base(context)
+    {
+    }
+
+    public override void EnterState()
+    {
+        _context._sword.XRGrabInteractable.selectEntered.AddListener(SwitchStateToOnlySwordHolded);
+        _context._xrGrabInteractable.selectEntered.AddListener(SwitchStateToOnlyScabbardHolded);
+    }
+
+    public override void ExitState()
+    {
+        _context._sword.XRGrabInteractable.selectEntered.RemoveListener(SwitchStateToOnlySwordHolded);
+        _context._xrGrabInteractable.selectEntered.RemoveListener(SwitchStateToOnlyScabbardHolded);
+    }
+
+    public override void UpdateState()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class NoSwordInside : SwordScabbardState
+{
+    public NoSwordInside(SwordAndScabbardInteractionSystem context) : base(context)
+    {
+    }
+
+    public override void EnterState()
+    {
+
+    }
+
+    public override void ExitState()
+    {
+
+    }
+
+    public override void UpdateState()
+    {
+        throw new System.NotImplementedException();
+    }
+}
